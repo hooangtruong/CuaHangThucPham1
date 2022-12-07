1a7a607b64406e337b92609db3c0a58c056bc229
truongduyen10092001@gmail.com <truongduyen10092001@gmail.com>
2022-12-08 00:46:22 +0700

    Upload Project First time

---
+++ DoAn_CuaHangThucPham/DoAn_CuaHangThucPham/Controllers/CuaHangController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_CuaHangThucPham.Controllers
+{
+    public class CuaHangController : Controller
+    {
+        // GET: CuaHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CuaHang/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CuaHang/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CuaHang/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CuaHang/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CuaHang/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CuaHang/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CuaHang/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DoAn_CuaHangThucPham/DoAn_CuaHangThucPham/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAn_CuaHangThucPham.MvcApplication" Language="C#" %>
